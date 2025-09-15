38e96b6501de17ea1be306dff38fbbaee3842c69
大野蓮 <oonoren@MacBook-Air-4.local>
2025-09-15 22:19:14 +0900

    Add CORS initializer

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,12 +1,13 @@
 # config/initializers/cors.rb
-require "rack/cors"
+allowed = ENV.fetch("ALLOWED_ORIGINS", "")
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins ENV.fetch("FRONTEND_ORIGIN", "http://localhost:5173")
-
+    origins(*allowed.split(",").map(&:strip).reject(&:empty?))
     resource "*",
       headers: :any,
-      methods: [:get, :post, :patch, :put, :delete, :options, :head]
+      methods: %i[get post put patch delete options head],
+      expose: %w[Authorization],
+      credentials: ENV["CORS_ALLOW_CREDENTIALS"] == "true"
   end
 end
