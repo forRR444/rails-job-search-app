7dca15c448dc754c216e27d219ea1aaff134e46a
大野蓮 <oonoren@MacBook-Air-4.local>
2025-09-16 19:39:35 +0900

    Update CORS settings in config/initializers/cors.rb

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -5,8 +5,8 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins origin
     resource "*",
-      headers: :any,                                   # ← 任意ヘッダOK（content-type等）
-      methods: %i[get post put patch delete options head],  # ← OPTIONS 必須
+      headers: :any,
+      methods: %i[get post put patch delete options head],
       max_age: 600
   end
 end
